40723968b2e6bfba8f27f0a700f7e4898106c852
quanyeyang <quanyemostima@gmail.com>
2026-09-20 04:52:51 -0700

    清理多余产物
    Signed-off-by: quanyeyang <quanyemostima@gmail.com>

--- .mostima.ko.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_mostima.ko := ld -r -m elf_x86_64 -z noexecstack --no-warn-rwx-segments --build-id=sha1  -T /usr/lib/modules/7.1.6-arch1-1/build/scripts/module.lds -o mostima.ko mostima.o mostima.mod.o .module-common.o
--- .mostima.mod.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_mostima.mod := printf '%s\n'   mostima.o | awk '!x[$$0]++ { print("./"$$0) }' > mostima.mod
